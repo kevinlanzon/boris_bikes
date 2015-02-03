5d136f0445cdaf5a0a137b0431fedf55b0d06d4c
Rizwan Ali <rizcube@gmail.com>
2015-02-03 12:39:05 +0000

    added second test in rspec, defined break method and introduced instance variable

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,18 @@
 class Bike
 
+	def initialize
+		@broken = false
+	end
+
+
+
+
 	def broken?
+		@broken
+	end
+
+	def break!
+		@broken = true
 	end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -6,4 +6,10 @@ describe Bike do
 		expect(the_bike).not_to be_broken
 	end
 
+	it 'should be able to break' do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
+	end
+
 end

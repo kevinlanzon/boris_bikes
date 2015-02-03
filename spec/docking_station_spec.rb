55b9c1a8fa81bac9533b55228623e4171f1f012d
Rizwan Ali <rizcube@gmail.com>
2015-02-03 19:59:47 +0000

    added container, created artificial class to test the rspect

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,44 +1,27 @@
-require 'docking_station'
+# require 'docking_station'
+require './lib/bike_container'
+
+class ContainerHolder; include BikeContainer; end
+
+describe BikeContainer do
+
+	let(:bike) { Bike.new }
+	let(:holder) { ContainerHolder.new }
+
+	it 'should accept a bike' do
+		expect(holder.bike_count).to eq(0)
+		holder.dock(bike)
+		expect(holder.bike_count).to eq(1)
+	end
+end
+require './lib/docking_station'
 
 describe DockingStation do
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new }
-	let(:station) { DockingStation.new(capacity: 20) }
 
-	def fill_station(station)
-		20.times { station.dock(Bike.new)}
+	let(:station) {DockingStation.new(capacity: 123) }
+
+	it 'should allow setting default capacity on initialising' do
+		expect(station.capacity).to eq(123)
+
 	end
-
-
-	it 'should eccept a bike' do
-		expect(station.bike_count).to eq(0)
-		station.dock(bike)
-		expect(station.bike_count).to eq(1)
-	end
-
-	it 'should release a bike' do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it 'should know when it\'s full' do
-		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
-		expect(station).to be_full	
-	end
-
-	it 'should not accept a bike if it\'s full' do
-		fill_station station
-		expect{ station.dock(bike) }.to raise_error(RuntimeError, 'Station is full')
-	end
-
-	it 'should provide the list of available bikes' do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break!
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes)
-	end
-
 end
